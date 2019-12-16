db1c2ab8b74babfe6e721259f0e529fd5c4e58b0
abhayrautela <abhayrautela@gmail.com>
2019-12-17 01:17:26 +0530

    Commit

---
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SessionController.java
@@ -0,0 +1,49 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SessionController {
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list() {
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id) {
+        return sessionRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        // also need to check children records before deleting
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        // because this is a PUT, we expect all attributes to be passed to method.
+        // in PATCH, only few attributes are updated
+        //TODO add validation that all all attributes are passed else return 400 bad payload error
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
+
+}
---
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
@@ -0,0 +1,49 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Speaker;
+import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakerController {
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list() {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable final Long id) {
+        // also need to check children records before deleting
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable final Long id, @RequestBody final Speaker speaker) {
+        // because this is a PUT, we expect all attributes to be passed to method.
+        // in PATCH, only few attributes are updated
+        //TODO add validation that all all attributes are passed else return 400 bad payload error
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+
+}
